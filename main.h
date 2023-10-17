f92466f8f755a08ceb45ab34a62741628a31e8ca
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-10-17 15:59:52 -0700

    testing

--- main.h
+++ main.h
@@ -14,5 +14,11 @@ int print_str(va_list list);
 int _strlen(char *n);
 int print_percent(void);
 int _strlencons(const char *n);
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_bin(va_list val);
+int print_unsigned(va_list args);
+int print_octa(va_list val);
+int print_hex(va_list val);
 
 #endif
